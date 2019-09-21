d0635f4c4bb0468fcf49b818bb5cbbc16f572dd4
Abhilash Bhargava <abhilash@Abhilashs-MacBook-Pro.local>
2019-09-21 16:05:26 +0530

    slick is working, fixed font family for all pages and created new files and links for footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,21 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
 import { ProductCategoryComponent } from './components/product-category/product-category.component'
 import { CheckoutComponent } from './components/checkout/checkout.component'
 import { ConfirmOrderComponent } from './components/confirm-order/confirm-order.component'
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { FAQComponent } from './components/faq/faq.component';
+import { SizeGuideComponent } from './components/size-guide/size-guide.component';
+import { ShippingComponent } from './components/shipping/shipping.component';
 
 const routes: Routes = [
 	{ path: '', component: HomepageComponent },
 	{ path: 'productDetail', component: ProductDetailComponent },
 	{ path: 'productCategory', component: ProductCategoryComponent },
 	{ path: 'checkout', component: CheckoutComponent },
-	{ path: 'confirmOrder', component: ConfirmOrderComponent }
+	{ path: 'confirmOrder', component: ConfirmOrderComponent },
+	{ path: 'aboutUs', component: AboutUsComponent },
+	{ path: 'faq', component: FAQComponent },
+	{ path: 'sizeGuide', component: SizeGuideComponent },
+	{ path: 'shippingPolicy', component: ShippingComponent }
 ];
 
 @NgModule({
