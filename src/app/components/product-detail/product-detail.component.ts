d5b875c5d43c74c48bd8735406645131dd898120
Milky Jain <milky.jain@aurigait.com>
2019-10-21 02:16:04 +0530

    changes on product detail page sizes and fit section - changes for the add to cart flow by adding fit and size

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ProductDetailService } from '../../services/product-detail.service';
+import { HomeService } from '../../services/home.service';
 import { environment } from 'src/environments/environment';
 
 declare var $:any;
@@ -14,15 +15,21 @@ var $scope;
 export class ProductDetailComponent implements OnInit {
   
   constructor( private productDetailServices : ProductDetailService,
-               private activatedRoute : ActivatedRoute
+               private activatedRoute : ActivatedRoute,
+               private homeservice: HomeService
    ) { }
 
   productDetail:any;
   product_id='';
   sizesAvailable:[];
+  fitsAvailable:[];
   containerLoaded = false;
   loc_type = localStorage.getItem('loc_type');
   indian_location = environment.india_location;
+  allSizes = [];
+  allFits = [];
+  selectedFit: any;
+  selectedSize: any;
 
   ngOnInit() {
   	$(document).foundation();
@@ -32,12 +39,32 @@ export class ProductDetailComponent implements OnInit {
       console.log(`${this.product_id}`);
     });
 
+    /* TODO : make it via ommon function at every page */
+    if(!this.loc_type) {
+      this.homeservice.getCountryFromIp().subscribe((ip_details)=>{
+        if(ip_details.countryCode == 'IN') {
+          var loc_type = environment.india_location;
+        } else {
+          var loc_type = environment.india_location;
+        }
+        localStorage.setItem('loc_type', loc_type);
+      });
+    }
+
     this.productDetailServices.getProductDetail(this.product_id).subscribe(data => {
       this.containerLoaded = true;
       if(data[0]) {
         this.productDetail=data[0];
         console.log(this.productDetail);
         this.sizesAvailable=this.productDetail.size.split(',');
+        this.fitsAvailable=this.productDetail.fit.split(',');
+        var types = {"type" : ["sizes", "categories", "fits"]};
+        this.homeservice.getAllEntities(types).subscribe((data)=>{
+          this.allSizes = data.sizes;
+          this.allFits = data.fits;
+          console.log(this.allSizes);
+          console.log(this.allFits);
+        });
       }
     });
   }
@@ -70,21 +97,27 @@ export class ProductDetailComponent implements OnInit {
   }
 
   addToCart(id) {
-    var cart_items = JSON.parse(localStorage.getItem('cart_items'));
-    if(cart_items) {
-      if(cart_items.includes(id)) {
-        alert('already added this product.');
+    if(!this.selectedSize) {
+      alert('Please select your size.');
+    } else if(!this.selectedFit) {
+      alert('Please select your fit.');
+    } else {
+      var cart_items = JSON.parse(localStorage.getItem('cart_items'));
+      if(cart_items) {
+        if(cart_items.find(item => item.id == id)) {
+          alert('already added this product.');
+        } else {
+          cart_items.push({id: id, size: this.selectedSize, fit: this.selectedFit});
+          localStorage.setItem('cart_items', JSON.stringify(cart_items));
+          alert('Successfully added the product.');
+        }
       } else {
-        cart_items.push(id);
+        let cart_items = <any[]> Array();
+        cart_items.push({id: id, size: this.selectedSize, fit: this.selectedFit});
         localStorage.setItem('cart_items', JSON.stringify(cart_items));
         alert('Successfully added the product.');
       }
-    } else {
-      let cart_items = <any[]> Array();
-      cart_items.push(id);
-      localStorage.setItem('cart_items', JSON.stringify(cart_items));
-      alert('Successfully added the product.');
+      console.log(localStorage.getItem('cart_items'));
     }
-    console.log(localStorage.getItem('cart_items'));
   }
 }
