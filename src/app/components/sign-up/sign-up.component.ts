ba4eb4a5ae58fb7fbe4d05faf2633f87e5835f84
Ayushi Jain <ayushijain649@gmail.com>
2020-02-10 21:35:16 +0530

    register api and name show on header just after register

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl, Validators, FormBuilder } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 
@@ -12,8 +12,10 @@ declare var $:any;
 export class SignUpComponent implements OnInit {
 
   constructor(public userService: UserService, public formBuilder: FormBuilder) { }
-
+  @Output() userSignedUp = new EventEmitter();
   ngOnInit() {
+    $('#signUpModal').foundation();
+    $('#verifyModal').foundation();
   }
   
   phone_error = '';
@@ -30,14 +32,15 @@ export class SignUpComponent implements OnInit {
     fname: ['', [Validators.required]],
     lname: ['', [Validators.required]],
     email: [''],
+    isd_code: '+91',
     phone_number: ['', [Validators.required, Validators.pattern(/^[0-9]*$/), Validators.minLength(7), Validators.maxLength(13)]]
   });
 
   verifyOtp() {
     var formdata = this.verifyOtpForm.value;
-    console.log(formdata);
+    //console.log(formdata);
     this.userService.verifyOtp(formdata).subscribe((data)=>{
-      console.log(data);
+      //console.log(data);
     });
   }
 
@@ -49,7 +52,7 @@ export class SignUpComponent implements OnInit {
 
   signup() {
     var formdata = this.signUpFormControl.value;
-    console.log(formdata);
+    //console.log(formdata);
     if(this.otp_field == 0) {
       this.userService.registerUser(formdata).subscribe((data)=>{
         this.otp_field = 1;
@@ -62,6 +65,7 @@ export class SignUpComponent implements OnInit {
         this.userDetails = data.data.userDetails;
         localStorage.setItem('user_details', JSON.stringify(data.data.userDetails));
         $('#signUpModal').foundation('close');
+        this.userSignedUp.emit(true);
         // TODO :  show successful register message
       }, (error:any) => {
         this.otp_error = error.message;
